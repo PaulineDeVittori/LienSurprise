bed25c665e656b40870c1bec736b74b00ce6a4b3
PaulineDeVittori <Pauline.de_Vittori.Etu@univ-lemans.fr>
2025-08-23 09:55:50 +0200

    version finale

--- lienJS.js
+++ lienJS.js
@@ -1,11 +1,13 @@
-const CODE_SECRET = "amourENSIM100224"; // ← Modifie le code secret ici
+const CODE_SECRET1 = "amourENSIM10022024calin"; // ← Modifie le code secret ici
+const CODE_SECRET2 = "amourensim10022024calin";
+const CODE_SECRET3 = "AMOURENSIM10022024CALIN";
 
 function verifierCode() {
   const saisie = document.getElementById("codeInput").value.trim();
   const message = document.getElementById("message");
   const lienSecret = document.getElementById("secret-link");
 
-  if (saisie === CODE_SECRET) {
+  if (saisie === CODE_SECRET1 || saisie === CODE_SECRET2 || saisie === CODE_SECRET3) {
     message.textContent = "";
     lienSecret.style.display = "block";
   } else {
